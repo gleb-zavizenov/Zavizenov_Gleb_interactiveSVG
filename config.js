e518d423ab20a296a9c7150f75fb34970f5c3ea2
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2019-11-23 14:22:39 -0500

    Finished setting up folder structure

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port:3306, // 8889 for mac, 3306 for windows
+    uname: "root",
+    pword: "", // leave blank for windows; root for mac
+    database: "db_poster"
+}
+
+module.exports = config;
---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+const sql = require('../utils/sql');
+
+router.get('/', (req, res) => {
+    // should really get the user data here and then fetch it thru, but let's try this asynchronously
+    //console.log('at the main route');
+
+    let query = "SELECT * FROM tbl_popup_data;";
+
+    sql.query(query, (err, result) => {
+        if (err) { //throw err; console.log(err);
+            console.log(err);
+        }
+
+        console.log(result); // should see objects wrapped in an array
+
+        // render the home view with dynamic data
+        res.render('home', { sections: result });
+    })
+})
+
+// router.get('/countries/:id', (req, res) => {
+//     // should really get the user data here and then fetch it thru, but let's try this asynchronously
+
+//     let query = `SELECT * FROM tbl_main_data WHERE ID="${req.params.id}"`;
+
+//     sql.query(query, (err, result) => {
+//         if (err) { throw err; console.log(err); }
+
+//         console.log(result[0]); // should see objects wrapped in an array
+
+//         // render the home view with dynamic data
+//         res.json(result[0]);
+//     })
+// })
+
+module.exports = router;
