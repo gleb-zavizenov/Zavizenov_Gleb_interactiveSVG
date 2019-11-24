549facf6ff5c6efa02d7469931f0d9c012a4452c
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2019-11-23 19:54:36 -0500

    Finished interactive flags

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     //console.log('at the main route');
 
-    let query = "SELECT * FROM tbl_main_data;";
+    let query = "SELECT * FROM tbl_popup_data;";
 
     sql.query(query, (err, result) => {
         if (err) { //throw err; console.log(err);
@@ -21,19 +21,19 @@ router.get('/', (req, res) => {
     })
 })
 
-// router.get('/countries/:id', (req, res) => {
-//     // should really get the user data here and then fetch it thru, but let's try this asynchronously
+router.get('/countries/:id', (req, res) => {
+    // should really get the user data here and then fetch it thru, but let's try this asynchronously
 
-//     let query = `SELECT * FROM tbl_main_data WHERE ID="${req.params.id}"`;
+    let query = `SELECT * FROM tbl_main_data WHERE ID="${req.params.id}"`;
 
-//     sql.query(query, (err, result) => {
-//         if (err) { throw err; console.log(err); }
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
 
-//         console.log(result[0]); // should see objects wrapped in an array
+        console.log(result[0]); // should see objects wrapped in an array
 
-//         // render the home view with dynamic data
-//         res.json(result[0]);
-//     })
-// })
+        // render the home view with dynamic data
+        res.json(result[0]);
+    })
+})
 
 module.exports = router;
